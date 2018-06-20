df381e0f9d071eefa5aec963897d941e818f7d51
Szymon Kaliski <hi@szymonkaliski.com>
2018-06-20 14:14:35 +0200

    engine = fetcher

--- fetcher/engine/crawlers/1.js
+++ fetcher/engine/crawlers/1.js
@@ -1,42 +1,40 @@
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
-const flatten = require('lodash.flatten');
-
-const toArray = list => [...list];
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const flatten = require("lodash.flatten");
 
 const getYearsUrls = async page => {
-  await page.goto('http://www.dziennikustaw.gov.pl/');
+  await page.goto("http://www.dziennikustaw.gov.pl/");
   return await page.evaluate(() => {
-    const elements = document.querySelector('#dnn_ctr617_ModuleContent > table > tbody').querySelectorAll('a');
+    const elements = document.querySelector("#dnn_ctr617_ModuleContent > table > tbody").querySelectorAll("a");
     return [...elements].map(a => ({ url: a.href, year: a.innerText }));
   });
 };
 
 const getPaginationIds = async page => {
   return await page.evaluate(() => {
-    const elements = document.querySelector('#dnn_ctr490_ViewJournal_trActsScope').querySelectorAll('a');
+    const elements = document.querySelector("#dnn_ctr490_ViewJournal_trActsScope").querySelectorAll("a");
     return [...elements].map(e => `#${e.id}`);
   });
 };
 
 const getPdfsFromPage = async (page, id) => {
   await page.click(id);
-  await page.waitForSelector('#dnn_ctr490_ViewJournal_wholeTable');
+  await page.waitForSelector("#dnn_ctr490_ViewJournal_wholeTable");
   const content = await page.content();
   const $ = cheerio.load(content);
   let result = [];
-  $('#dnn_ctr490_ViewJournal_wholeTable table tbody tr').each((i, element) => {
+  $("#dnn_ctr490_ViewJournal_wholeTable table tbody tr").each((i, element) => {
     if (i !== 0) {
-      const tds = $(element).find('td');
+      const tds = $(element).find("td");
       const title = $(tds.get(1))
-        .children('a')
+        .children("a")
         .text()
         .trim();
       const url = $(tds.get(2))
-        .children('a')
-        .attr('href');
+        .children("a")
+        .attr("href");
       const date = $(tds.get(3))
-        .children('span')
+        .children("span")
         .text();
       result.push({
         title,
@@ -53,7 +51,7 @@ const getAllPdfsUrls = async (browser, page) => {
   const pagination = await getPaginationIds(page);
   const pdfs = pagination.map(async p => {
     const newPage = await browser.newPage();
-    await newPage.goto('http://www.dziennikustaw.gov.pl/DU/2018');
+    await newPage.goto("http://www.dziennikustaw.gov.pl/DU/2018");
     return await getPdfsFromPage(newPage, p);
   });
   return Promise.all(pdfs);

e407f90372db27c0a17dbda670486f5ef0dc961d
Szymon Kaliski <hi@szymonkaliski.com>
2018-08-11 16:57:54 -0700

    uokik.js

--- fetcher/src/crawlers/uokik.js
+++ fetcher/src/crawlers/uokik.js
@@ -1,17 +1,12 @@
 const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-const { flatten } = require("lodash");
-const async = require("async");
-const fs = require("fs");
-
-const logger = require("../logger");
-const { simpleDOMListParser, simpleDOMGet } = require("../utils");
+const { simpleDOMListParser } = require("../utils");
+const { EventEmitter } = require("events");
 
 const BASE_URL = "http://www.uokik.gov.pl";
 const MAIN_URL = `${BASE_URL}/dzienniki_urzedowe_uokik2.php`;
-const SOURCE_NAME = "uokik.gov.pl";
+const SOURCE_NAME = "Dzienniki Urzędowe UOKiK";
 
-const crawl = async () => {
+const crawl = async emitter => {
   const browserOpts = {
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
@@ -21,7 +16,7 @@ const crawl = async () => {
 
   const ITEM_SELECTOR = "#subpage > div > div > ul > li";
 
-  return new Promise(async (resolve, reject) => {
+  return new Promise(async resolve => {
     const page = await browser.newPage();
     await page.goto(MAIN_URL, { waitUntil: "networkidle0" });
 
@@ -31,14 +26,19 @@ const crawl = async () => {
         .text()
         .trim(),
       url: BASE_URL + node.find("a").attr("href"),
-      source: SOURCE_NAME,
+      sourceName: SOURCE_NAME,
       date: `01-0${node
         .find("a")
         .text()
         .trim()
         .slice(-6)
         .replace("/", "-")}`
+        .split("-")
+        .reverse()
+        .join("-")
     }));
+
+    emitter.emit("entity", list);
 
     resolve(list);
 
@@ -46,7 +46,8 @@ const crawl = async () => {
   });
 };
 
-module.exports = async () => {
-  const listOfPdfs = await crawl();
-  return listOfPdfs;
+module.exports = () => {
+  const emitter = new EventEmitter();
+  crawl(emitter);
+  return emitter;
 };

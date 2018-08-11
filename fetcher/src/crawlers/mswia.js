03e5bcc4eddbb84038a2ddd292c14c190f202000
Szymon Kaliski <hi@szymonkaliski.com>
2018-08-11 16:11:23 -0700

    mswia.js

--- fetcher/src/crawlers/mswia.js
+++ fetcher/src/crawlers/mswia.js
@@ -3,18 +3,18 @@ const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 const { flatten } = require("lodash");
 const async = require("async");
-const fs = require("fs");
+const { EventEmitter } = require("events");
 
 const logger = require("../logger");
 
 const MAIN_URL = "http://edziennik.mswia.gov.pl/";
-const SOURCE_NAME = "mswia";
+const SOURCE_NAME = "Dziennik Urzędowy Minister Spraw Wewnętrznych i Administracji";
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const crawl = async () => {
+const crawl = async emitter => {
   const browserOpts = {
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
@@ -72,7 +72,8 @@ const crawl = async () => {
           content = await newPage.content();
           $ = cheerio.load(content);
         }
-        return flatten(
+
+        const items = flatten(
           $(ITEM_SELECTOR)
             .map((i, d) => {
               const title =
@@ -84,21 +85,38 @@ const crawl = async () => {
               const date = $(d)
                 .find("td.acts__publish-date.ng-binding")
                 .text()
-                .trim();
-              const updatedate = $(d)
+                .trim()
+                .split(".")
+                .reverse()
+                .join("-");
+              const updateDate = $(d)
                 .find("td.acts__date > div > div:nth-child(1) > span")
                 .text()
-                .trim();
-              const source = "cba";
+                .trim()
+                .split(".")
+                .reverse()
+                .join("-");
               const url =
                 MAIN_URL +
                 $(d)
                   .find("td.acts__pdf.text-right > a")
                   .attr("href");
-              return { title, date, update_date: updatedate, source, url, source: SOURCE_NAME, ocr: false };
+
+              return {
+                title,
+                date,
+                updateDate,
+                url,
+                sourceName: SOURCE_NAME,
+                ocr: false
+              };
             })
             .get()
         );
+
+        emitter.emit("entity", items);
+
+        return items;
       },
       async (err, results) => {
         await browser.close();
@@ -111,7 +129,8 @@ const crawl = async () => {
   });
 };
 
-module.exports = async () => {
-  const listOfPdfs = await crawl();
-  return listOfPdfs;
+module.exports = () => {
+  const emitter = new EventEmitter();
+  crawl(emitter);
+  return emitter;
 };

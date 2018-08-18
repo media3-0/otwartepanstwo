eca5b2f0ffe1ba16c9388f1c6a61fe958f14cf51
Szymon Kaliski <hi@szymonkaliski.com>
2018-08-18 08:40:38 -0700

    dockerignore for docker files

--- scripts/build.prod.sh
+++ scripts/build.prod.sh
@@ -3,9 +3,4 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR/.." || exit 1
 
-# move data tiles out of shared volume for faster build
-mv docker/data/files ../
-
 docker-compose -f docker/docker-compose.prod.yml build
-
-mv ../files docker/data/

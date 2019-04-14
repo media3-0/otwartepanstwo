b8814a3b3606dde657fb1a37f87fb4394912704a
Jan Hoffmann <jan@hfmn.pl>
2019-04-14 14:31:23 +0200

    node cron fixes

--- fetcher/index.js
+++ fetcher/index.js
@@ -1,16 +1,18 @@
-const CronJob = require("cron").CronJob;
+const cron = require("node-cron");
 
 const fetcher = require("./src/index");
 
 const IS_DEV = process.env.NODE_ENV === "development";
 
-const job = new CronJob({
-  cronTime: "00 00 */1 * *",
-  onTick: function() {
-    fetcher();
-  },
-  start: false,
-  timezone: "Europe/Warsaw"
-});
-
-IS_DEV ? fetcher() : job.start();
+if (IS_DEV) {
+  fetcher();
+} else {
+  cron.schedule({
+    cronTime: "00 00 */1 * *",
+    onTick: function() {
+      fetcher();
+    },
+    start: false,
+    timezone: "Europe/Warsaw"
+  });
+}

26a548270d55b27855c8b46e5b00f5b430a0e1b9
Szymon Kaliski <hi@szymonkaliski.com>
2019-04-22 13:40:55 +0200

    index for lower case column

--- db/init.sh
+++ db/init.sh
@@ -28,6 +28,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   );
 
   CREATE EXTENSION pg_trgm;
+  CREATE INDEX idx_documents_content_lower ON documents USING gin (content_lower gin_trgm_ops);
   CREATE INDEX idx_documents_content ON documents USING gin (content gin_trgm_ops);
   CREATE INDEX idx_documents_title ON documents USING gin (title gin_trgm_ops);
 EOSQL

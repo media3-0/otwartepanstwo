4af3a9378c983a6d434678c969d7f1c924c32c49
Szymon Kaliski <hi@szymonkaliski.com>
2019-09-30 13:40:16 +0200

    swagger docs

--- public-api/index.js
+++ public-api/index.js
@@ -1,15 +1,24 @@
-const changeCaseKeys = require("change-case-keys");
 const express = require("express");
 const knex = require("knex");
 const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-
-const paginator = require("./knex-paginate");
-
-const toClient = obj => changeCaseKeys(obj, "camelize");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const PORT = process.env.PORT || 5000;
-const DOCUMENTS_TABLE = "documents";
+
+const createV1 = require("./v1");
+
+const specs = swaggerJsdoc({
+  swaggerDefinition: {
+    info: {
+      title: "OtwartePaństwo API",
+      description: "API jest limitowane do 100 zapytań w ciągu 10 minut z tego samego hosta.",
+      version: "1.0.0"
+    },
+    basePath: "/public-api/"
+  },
+  apis: ["v1.js"]
+});
 
 const createDB = () =>
   new Promise((resolve, reject) => {
@@ -38,57 +47,11 @@ const init = async () => {
   app.use(express.json());
   app.set("trust proxy", 1); // needed for express-rate-limit
 
-  // v1
-  const v1RateLimiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 100 });
+  // v1 api
+  app.use("/v1", createV1({ db }));
 
-  app.get("/v1/sources", v1RateLimiter, (req, res) => {
-    db(DOCUMENTS_TABLE)
-      .distinct()
-      .pluck("source_name")
-      .then(sources => res.json(toClient(sources)));
-  });
-
-  app.get("/v1/documents/", v1RateLimiter, (req, res) => {
-    const { search, dateFrom, dateTo, sourceName, hash, page, perPage, sortBy, sortDirection } = req.query;
-    const fields = ["title", "date", "last_download", "source_name", "hash", "url"];
-
-    let query = db(DOCUMENTS_TABLE).select(...fields);
-
-    if (hash) {
-      query = query.where("hash", "=", hash);
-    }
-
-    if (dateFrom) {
-      query = query.where("date", ">", dateFrom);
-    }
-
-    if (dateTo) {
-      query = query.where("date", "<", dateTo);
-    }
-
-    if (sourceName) {
-      query = query.where("source_name", "=", sourceName);
-    }
-
-    if (search) {
-      query = query.where(db.raw(`content_lower LIKE '%${search}%'`));
-    }
-
-    if (sortBy) {
-      query = query.orderBy(sortBy, sortDirection);
-    }
-
-    paginator(db, query, { page: page ? parseInt(page) : 1, perPage: Math.min(perPage || 20, 100) }).then(
-      ({ pagination, data }) =>
-        res.json(
-          toClient({
-            page: pagination.currentPage,
-            totalPages: pagination.lastPage,
-            data
-          })
-        )
-    );
-  });
+  // docs
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
   app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
